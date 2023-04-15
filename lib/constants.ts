bbcd372443ad8144ab31cedbb93950cfd9a2d1bf
BowTiedHeron <bowtiedheron@protonmail.com>
2023-04-15 18:47:29 +0200

    quick config changes while exploring/setting up

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,30 +14,37 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'https://demo.vercel.events';
+export const SITE_URL = 'https://web3-event-kit.vercel.app';
+// TODO: add SITE_ORIGIN
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
-export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const TWITTER_USER_NAME = 'd_d_agency_web3';
+export const BRAND_NAME = 'D_D Agency';
+export const SITE_NAME_MULTILINE = ['Agency', 'Conf'];
+export const SITE_NAME = 'Agency Conf';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'This is an open source project that Next.js developers can clone, deploy, and fully customize for web3 native events. Created through collaboration of marketers, designers, and developers at Vercel with a web3 touch from D_D Agency.';
+
+export const TWEET_TEXT = META_DESCRIPTION;
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
+
+// TODO: replace dates
 export const DATE = '01 January 2022';
 export const SHORT_DATE = 'Jan 1 - 9:00am PST';
 export const FULL_DATE = 'Jan 1st 9am Pacific Time (GMT-7)';
-export const TWEET_TEXT = META_DESCRIPTION;
+
+// TODO: decide what to do with cookies
 export const COOKIE = 'user-id';
 
-// Remove process.env.NEXT_PUBLIC_... below and replace them with
+// TODO: Remove process.env.NEXT_PUBLIC_... below and replace them with
 // strings containing your own privacy policy URL and copyright holder name
 export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
 export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
+// TODO: Add link to CODE_OF_CONDUCT
 export const CODE_OF_CONDUCT =
   'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
-export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
+export const REPO = 'https://github.com/D-D-Agency/web3-event-kit';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
@@ -45,13 +52,13 @@ export const NAVIGATION = [
     route: '/stage/a'
   },
   {
-    name: 'Vercel Stage',
+    name: 'Agency Stage',
     route: '/stage/c'
   },
-  {
-    name: '100ms Stage',
-    route: '/stage/m'
-  },
+  // {
+  //   name: '100ms Stage',
+  //   route: '/stage/m'
+  // },
   {
     name: 'Schedule',
     route: '/schedule'
